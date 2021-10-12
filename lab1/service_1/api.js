2adc2da92dd47462b21f5e07775f292c14b82632
La7rodectus <maxmega27@gmail.com>
2021-10-12 14:27:07 +0300

    + laba

---
+++ lab1/service_1/api.js
@@ -0,0 +1,18 @@
+'use strict';
+const http = require('http');
+const PORT = process.env.port || 8080;
+const URL = 'http://localhost:';
+
+const execReq = async (req, res) => {
+  console.log('New API REQ:', req.method, req.url);
+
+  res.setHeader('Content-Type', 'application/json');
+
+  res.end('Hello from service 1');
+};
+
+http.createServer(async (req, res) => {
+  execReq(req, res);
+}).listen(PORT);
+
+console.log(`Api listen on ${URL}${PORT}`);
