812a888563c3e614807e435eea6bce4074d69fd3
La7rodectus <maxmega27@gmail.com>
2021-11-10 10:52:55 +0200

    final

--- lab3/services/dbc_service/secret.js
+++ lab3/services/dbc_service/secret.js
@@ -1,10 +1,17 @@
 
+// const dbConnData = {
+//   host: 'sql11.freemysqlhosting.net',
+//   user: 'sql11450005',
+//   password: 'IyQZbHAsAd',
+//   database: 'sql11450005'
+// };
+
 const dbConnData = {
-  host: 'sql11.freemysqlhosting.net',
-  user: 'sql11450005',
-  password: 'IyQZbHAsAd',
-  database: 'sql11450005'
-};
+    host: 'mysql-service',
+    user: 'newuser',
+    password: 'password',
+    database: 'EmailsDB'
+  };
 
 module.exports = {
   dbConnData,
